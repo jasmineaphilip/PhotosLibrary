dc2fd308bec9a79dbd694807a97a922718e03cd2
Radhe Bangad <radabeee@Radhes-MacBook-Air.local>
2019-11-16 11:12:56 -0500

    added model

---
+++ Photos26/src/model/Album.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.io.*;
+import java.util.*;
+
+public class Album {
+
+	private String name;
+	private List<Photo> photos;
+	private Photo oldestPhoto;
+	private Photo earliestPhoto;
+	
+	public Album() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Photos26/src/model/Photo.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.io.*;
+import java.util.*;
+import java.util.Calendar;
+
+import javafx.scene.image.Image;
+
+public class Photo {
+	
+	private Image image;
+	private String caption;
+	//private List<Tag> tags;
+	private Calendar cal;
+
+	public Photo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
