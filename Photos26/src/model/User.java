a9557fd3873f23c99ad809f4f339d214640b967b
Radhe Bangad <radabeee@Radhes-Air.fios-router.home>
2019-11-16 18:12:03 -0500

    admin add and delete users

--- Photos26/src/model/User.java
+++ Photos26/src/model/User.java
@@ -3,12 +3,32 @@ package model;
 import java.io.Serializable;
 
 public class User implements Serializable{
-	private String username;
 	private String fullname;
+	private String username;
 	
-	public User(String username, String fullname) {
-		this.username = username;
+	public User(String fullname, String username) {
+		this.setFullname(fullname);
+		this.setUsername(username);
+	}
+	
+	public String toString() {
+		return getFullname() + " - " + getUsername();
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
 		this.fullname = fullname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 }
