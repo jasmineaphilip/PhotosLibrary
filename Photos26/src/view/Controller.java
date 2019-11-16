dfd43f99b190dcf95daf1dff9f8589a8f5e48a27
Radhe Bangad <radabeee@Radhes-MacBook-Air.local>
2019-11-16 01:57:53 -0500

    stock username+photos

--- Photos26/src/view/Controller.java
+++ Photos26/src/view/Controller.java
@@ -3,21 +3,30 @@ package view;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Controller {
 	
+	@FXML Button loginButton;
+	@FXML TextField usernameField;
+	
 	public void login(ActionEvent event) throws IOException {
-		Parent loginParent = FXMLLoader.load(getClass().getResource("another.fxml"));
-		Scene stock = new Scene(loginParent);
-		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-		
-		window.setScene(stock);
-		window.show();
+		String username = usernameField.getText();
+		if(username.equals("stock")) {
+			Parent loginParent = FXMLLoader.load(getClass().getResource("another.fxml"));
+			Scene stock = new Scene(loginParent);
+			Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+			
+			window.setScene(stock);
+			window.show();
+		}
 	}
 
 	public void start(Stage mainStage) {
