daa9b136121ba51d7b0187ef2d31b67af3b95a60
Radhe Bangad <radabeee@Radhes-Air.fios-router.home>
2019-11-16 16:06:35 -0500

    adminusercontroller

--- Photos26/src/view/AdminUserController.java
+++ Photos26/src/view/AdminUserController.java
@@ -1,10 +1,26 @@
 package view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.User;
 
 public class AdminUserController {
+	
+	@FXML TextField createUsernameField;
+	@FXML TextField createFullnameField;
+	@FXML TextField deleteUsernameField;
+	@FXML TextField deleteFullnameField;
+	@FXML Button createUserButton;
+	@FXML Button deleteUserButton;
+	
+	private ObservableList<User> users;
+	
 	public void start(Stage mainStage) {
-		// TODO Auto-generated method stub
+		users = FXCollections.observableArrayList();
 		
 	}
 
